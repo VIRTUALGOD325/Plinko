93d9b5f502bf9878ceac6e93b24da065cd9e10e8
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2020-08-28 16:59:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,79 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
 
-var ground, topGround;
-
+var divisionHeight=300;
+var score =0;
 function setup() {
-  createCanvas(480,800);
-  createSprite(400, 200, 50, 50);
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
 
-  ground = new Ground(240,780,480,30);
-  topGround = new Ground(240,750,480,30);
 
-  division1 = new Division(0,750,20,40);
-  division2 = new Division(70,750,20,40);
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
 
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
 }
+ 
+
 
 function draw() {
-  background(255,255,255);  
-
-  ground.display();
-  topGround.display();
-
-  division1.display();
-  division2.display();
-
-  drawSprites();
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
 }
